e82f074dc3806086c42a6f6af1db6eda84d7de80
Wojtek <ws420@ic.ac.uk>
2024-05-16 13:16:09 +0100

    specified R dependencies, created initial dockerfile

--- qc-requirements-bioc.R
+++ qc-requirements-bioc.R
@@ -1,22 +1,5 @@
 bioc_pkgs<-c(
-  'batchelor',
-  'BiocGenerics',
-  'BiocStyle',
-  'biomaRt',
-  'DelayedArray',
-  'DelayedMatrixStats',
   'DropletUtils',
-  'edgeR',
-  'GenomicRanges',
-  'graph',
-  'IRanges',
-  'limma',
-  'MAST',
-  'multtest',
-  'preprocessCore',
-  'rhdf5',
-  'S4Vectors',
-  'scater',
   'SingleCellExperiment',
   'SummarizedExperiment'
 )
